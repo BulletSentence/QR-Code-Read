1e5db1686c8d7167181b35119c96ab168c7682a9
Leonardo Almeida de Araújo <37451620+BulletSentence@users.noreply.github.com>
2020-04-28 16:05:34 -0300

    QR reader implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home: HomePage(),
 ));
 
-class HomePage extends StatelessWidget  {
-    @override
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = " ";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission Failed";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error: $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "Scan Failed";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error: $ex";
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("QR Scanner"),
+        title: Text("QR Scanner BS"),
       ),
       body: Center(
-        child: Text("Olá Mundo",
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.camera),
-          label: Text("Scan QR"),
-          onPressed: (){},
-          ),
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan QR Code"),
+        onPressed: _scanQR,
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
